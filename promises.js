17c784a025544e9d70a5cebab6727285a197b003
TheEsquel <rem.asadullin@gmail.com>
2019-02-14 02:50:23 +0500

    promises were changed

--- promises.js
+++ promises.js
@@ -1,22 +1,40 @@
-window.onload = function() {
+//  принимает на первый параметр   сагу  ,  должна отрабатывать на каждый диспатч
 
-   let promise = new Promise((resolve, reject) => {
-       this.setTimeout(()=>{
-            //  если успех
-        resolve('Promise works!');
-            //  если ошибка
-            
-
-       }, 3000 );
-   });
-
-   promise
-        .then(result => {
-            console.log('resolve()', result);
-        },
-        error => {
-            console.log('reject()', error);
-        },
-    );
-
+function* saga () {
+    yield  takeEvery('ACTION_TYPE', fetchSomething);
 }
+
+
+const takeEvery = (saga, ...args) =>  fork(function*() {
+   while(true){
+     const action = yield take(saga)
+     yield fork (saga, ...args.concat(action))
+   }
+})
+
+yield delay(5);
+
+
+const delay =async (sec) =>  { 
+   yield call(test, sec);
+} 
+
+async function test (sec) {
+   return await new Promise (function (resolve, reject){
+      
+       setTimeout(()=> resolve(), sec*1000);
+   })
+}
+
+// Промис ходит за файлом и показывает console.log()
+
+// Написать промисы без async который ходит за файлом  file.jpg, если запрос успешным то идет за файлов file2.jpg, если ок, то пишет console.log('ok')
+
+// промис который принимает файл, ждет 5 секунд и потом идет за файлом, возвращает результат получения файла
+
+// промис который кидает ошибку через N секунд
+
+const errorPromise = new Promise(function(resolve, reject){
+    setTimeout(() => reject(new Error('error')))
+})
+errorPromise()
